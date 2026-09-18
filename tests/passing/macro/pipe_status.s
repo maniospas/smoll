47b5466a7695dc9bddf3cb443c98c93efe004728
maniospas <maniospas@hotmail.com>
2026-09-19 00:18:55 +0300

    reinstated tests in ci

--- tests/passing/macro/pipe_status.s
+++ tests/passing/macro/pipe_status.s
@@ -11,18 +11,17 @@ def pair(float _x, float _y)
     return class (x, y)
 
 def foo(pipe obj)
-    p = mut macro<match> (@open(obj reader), pair::name)
+    p = mut macro<match> (@open(obj writer), pair::name)
     p.x = 3.0
     unsafe_console().print "ended"
 
 def main()
     CLI = edit console()
-    INCORRUPTIBLE = edit arena(1024 incorruptible)
-    THREADS = edit growing_thread_pool cpu 16
+    THREADS = edit growing_thread_pool(cpu 16, 1024)
 
     obj = mut macro<shared> @pair(1.0,2.0)
     t = mut thread(type foo, obj)
 
     join t # synchronize by waiting for thread to reaease its resources
-    if try p = mut macro<match> (@open(obj reader), pair::name)
+    if try p = macro<match> (@open(obj reader), pair::name)
         print compiler::deref p.x
